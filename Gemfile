6ec120467de153873d198cff41f19b96fdaa7547
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2010-10-05 22:01:38 -0500

    rails new js.app

--- Gemfile
+++ Gemfile
@@ -1,3 +1,6 @@
-source :rubygems
+source 'http://rubygems.org'
 
 gem 'rails', '3.0.0'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'ruby-debug'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsApp::Application.config.secret_token = '72cbd0b02af4ac3fec853b0935ba4d1d70f4f88f0f75e0795bc4800f3120f2ad89efe4759a1e263fa150603d72500a023cca802ded5b892426b9004a14a1ffb2'
